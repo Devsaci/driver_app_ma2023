b65416c17d8bbe849670b32b5ab80400e87d3825
saci zakaria <sacideveloppeur@gmail.com>
2022-12-14 19:34:42 +0100

    static void restartApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Driver_App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to Driver App"),
+  runApp(
+    MyApp(
+      child: MaterialApp(
+        title: 'Driver_App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Welcome to Driver App"),
+          ),
         ),
       ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  final Widget? child;
+
+  const MyApp({super.key, this.child});
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child!,
     );
   }
 }
