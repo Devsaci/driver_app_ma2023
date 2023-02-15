32b773380762bd7f1e3fc51ae61aabdd2df6c88c
saci zakaria <sacideveloppeur@gmail.com>
2023-02-15 12:38:54 +0100

    SingleChildScrollView / Padding

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -14,32 +14,34 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal.shade900,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Image.asset("images/logo1.png"),
-            ),
-            const Text(
-              'Register as a Driver',
-              style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              controller: nameTextEditingController,
-              style: const TextStyle(color: Colors.grey),
-              decoration: const InputDecoration(
-                labelText: "Name",
-                labelStyle: TextStyle(color: Colors.grey,fontSize: 14),
-                hintText: "Name",
-                hintStyle: TextStyle(color: Colors.grey,fontSize: 14),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Image.asset("images/logo1.png"),
               ),
-            ),
-          ],
+              const Text(
+                'Register as a Driver',
+                style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                controller: nameTextEditingController,
+                style: const TextStyle(color: Colors.grey),
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  labelStyle: TextStyle(color: Colors.grey,fontSize: 14),
+                  hintText: "Name",
+                  hintStyle: TextStyle(color: Colors.grey,fontSize: 14),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
