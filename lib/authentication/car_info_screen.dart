8d12991bb1ae49415bb392e7459d9f7b03186081
saci zakaria <sacideveloppeur@gmail.com>
2023-02-22 10:02:27 +0100

    Text

--- lib/authentication/car_info_screen.dart
+++ lib/authentication/car_info_screen.dart
@@ -11,12 +11,20 @@ class _CarInfoScreenState extends State<CarInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.teal.shade900,
       body: Column(
         children: [
           Padding(
             padding: EdgeInsets.all(20.0),
             child: Image.asset("images/logo1.png"),
+          ),
+          const Text(
+            "Write Car Details",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
           )
         ],
       ),
